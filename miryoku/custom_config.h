8c2501221fd281434bd2e6eee70147c15820683a
anjerukare <anjerukare@gmail.com>
2024-01-12 23:41:25 +0700

    Replace Backspace in Tap layer to goto Base layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -28,4 +28,4 @@ U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp 
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SEMI,          \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &to U_BASE,        &kp DEL,           U_NP,              U_NP
